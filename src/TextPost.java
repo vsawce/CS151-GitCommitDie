51c86e9ae6dbf54581142d06dcf11163585c70b5
vsawce <vsaw30@gmail.com>
2023-10-17 01:00:30 -0700

    Initial version of TextPost

---
+++ src/TextPost.java
@@ -0,0 +1,12 @@
+public class TextPost {
+
+    private String postContents;
+
+    public String   getTextPost() {
+        return postContents;
+    }
+    public void     setTextPost(String tp) {
+        postContents = tp;
+    }
+    
+}
