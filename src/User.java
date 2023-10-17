a441f7ca4629eae5d3be82210132886feec76c56
vsawce <vsaw30@gmail.com>
2023-10-17 01:10:56 -0700

    Initial version of User

--- src/User.java
+++ src/User.java
@@ -1,3 +1,40 @@
 public class User {
-    
+    private String  name; //
+    private String  password; // 
+    private boolean authStatus; //
+
+    public          User(String nm, String pwd) {
+        name = nm;
+        password = pwd;
+    }
+
+    public String   getName() {
+        return name;
+    }
+    private String  getPassword() {
+        return password;
+    }
+    public boolean  getAuthStatus() {
+        return authStatus;
+    }
+
+    // private void    setName(String newName) {
+    //     name = newName;
+    // }
+    // private void    setPassword(String newPass) {
+    //     password = newPass;
+    // }
+    private void    setAuthStatus(boolean newAuth) { //
+        authStatus = newAuth;
+    }
+
+    public boolean  checkPassword(String pwAttempt) {
+        boolean passCorrect;
+
+        if (pwAttempt == getPassword()) { passCorrect = true; }
+        else                            { passCorrect = false; }
+
+        setAuthStatus(passCorrect);
+        return passCorrect;
+    }
 }
