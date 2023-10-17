2874e80283a919c5371aeb292a707aba0977ff43
vsawce <vsaw30@gmail.com>
2023-10-17 01:59:18 -0700

    Compilation testing code

--- src/Runner.java
+++ src/Runner.java
@@ -4,6 +4,16 @@
 public class Runner { 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        //Test code for compilation testing purposes
+        User nerd = new User("vsawce", "123");
+        Post p = new Post(nerd, "r/tifu blah blah blah");
+        System.out.println("----------------");
+        System.out.println("Username: " + p.getUser().getName());
+        System.out.println("Date: " + p.getDate());
+        System.out.println("Post Id: " + p.getPostId());
+        System.out.println("\n"+p.getTextPostContents());
+        System.out.println("----------------");
+
+
     } 
 }
