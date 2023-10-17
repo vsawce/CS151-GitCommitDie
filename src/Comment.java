592ee474a44e65feeeb7aa78d26b74aef1af8f31
vsawce <vsaw30@gmail.com>
2023-10-17 00:58:55 -0700

    Initial version of Comment class

--- src/Comment.java
+++ src/Comment.java
@@ -1,3 +1,19 @@
 public class Comment {
+
+    private String commentText;
+    private int commentId;
+
+    public String   getCommentText() {
+        return commentText;
+    }
+    public int      getCommentId() {
+        return commentId;
+    }
+    public void     setCommentText(String ct) {
+        commentText = ct;
+    }
+    public void     setCommentId(int cid) {
+        commentId = cid;
+    }
     
 }
