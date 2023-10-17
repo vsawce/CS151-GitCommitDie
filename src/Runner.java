e19ac55ee11e23323aedc43879444ddc0b2e19d9
vsawce <vsaw30@gmail.com>
2023-10-17 00:51:05 -0700

    Hello world

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+// Hello World Template
+// FileName : "HelloWorld.java". 
+  
+public class Runner { 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+}
