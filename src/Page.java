7598d384849a69024d27faf7744e0b4e2432e5ab
Victoria Le <95896163+levictoria0117@users.noreply.github.com>
2023-10-17 22:42:35 -0700

    update page

--- src/Page.java
+++ src/Page.java
@@ -1,3 +1,34 @@
 public class Page {
-    
+    //private int     pageType;
+    private User     currUser;
+    private TextPost currPost;
+    private Comment  currComment;
+
+    //private void setPageType(int pageType) { this.pageType = pageType; }
+    void setCurrUser(User currUser)          { this.currUser = currUser; }
+    void setCurrPost(TextPost currPost)      { this.currPost = currPost; }
+    void setCurrComment(Comment currComment) { this.currComment = currComment;}
+
+    //private int getPageType()  { return pageType; }
+    //User getCurrUser()         { return currUser; }
+    //TextPost getCurrPost()     { return currPost; }
+
+    String displayCurrUser()     { return currUser.getName(); }
+    String displayCurrComment() {
+        return currComment.displayComment(currComment.getPostID(),
+                currUser.getName(), currComment.getCommentText(),
+                currComment.getDate(), currComment.getTime());};
+    String displayCurrPost() {
+        return currPost.displayPost(currPost.getPostID(),
+                currUser.getName(), currPost.getTextPost(),
+                currPost.getDate(), currPost.getTime());
+    }
+
+    void displayPage(){
+        System.out.println("----- Display Page -----");
+        System.out.println("User: " + displayCurrUser());
+        displayCurrPost();
+        displayCurrComment();
+        System.out.println("------------------------");
+    }
 }
