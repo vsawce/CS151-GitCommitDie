d8dc1568212a27f965dbe5fbb243c5a350eb86c0
Victoria Le <95896163+levictoria0117@users.noreply.github.com>
2023-10-17 22:44:13 -0700

    update textpost

--- src/TextPost.java
+++ src/TextPost.java
@@ -1,4 +1,18 @@
-public class TextPost {
+public class TextPost extends Post{
+    String postContents;
+    public TextPost(String post) { postContents = post; }
+    void setTextPost(String postContents){ this.postContents = postContents; }
+    String getTextPost() { return postContents; }
+
+    String displayPost(int id, String name, String post, String date, String time){
+        String postInfo = ("PostID: " + id + "\n" + "@" + name + "\n"
+                + post + "\n" + date + " " + time + "\n");
+        System.out.println(postInfo);
+        return postInfo;
+    }
+}
+
+/**public class TextPost {
 
     private String postContents; //
 
@@ -13,4 +27,4 @@ public class TextPost {
         postContents = tp;
     }
     
-}
+}**/
