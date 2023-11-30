10795fd755f7b2fbbdbd901c515902baedd5a919
vsawce <vsaw30@gmail.com>
2023-11-30 01:48:22 -0800

    Retrofitted sort functions to reflect sorting methodology for epoch time

--- src/Page.java
+++ src/Page.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 
 public class Page {
@@ -39,24 +40,31 @@ public class Page {
         System.out.println("------------------------");
     }
 
-    void sortByTime(){
-        
-        Collections.sort(posts, (n1,n2) -> {
-
-            Date Date1 = new Date();
-            Date Date2 = new Date();
-
-            return Date1.compareTo(Date2);
-        } );  
-    }
-    void sortByKarma(){
-
-        Collections.sort(posts, (n1,n2) -> {
-
-            return Integer.compare(n1.getKarma(), n2.getKarma());
+    void sortByTime(boolean ascending){
+        Collections.sort(posts, new Comparator<Post>() {
+            @Override
+            public int compare(Post n1, Post n2) {
+                //Ascending order
+                if (ascending) {
+                    return Long.compare(n1.getTime(), n2.getTime());
+                }
+                //Descending order (n1/n2 swapped)
+                return Long.compare(n2.getTime(), n1.getTime());
+            }
         });
-        
-
+    }
+    void sortByKarma(boolean ascending){
+        Collections.sort(posts, new Comparator<Post>() {
+            @Override
+            public int compare(Post n1, Post n2) {
+                //Ascending order
+                if (ascending) {
+                    return Integer.compare(n1.getKarma(), n2.getKarma());
+                }
+                //Descending order (n1/n2 swapped)
+                return Integer.compare(n2.getKarma(), n1.getKarma());
+            }
+        });
         
     }
 }
